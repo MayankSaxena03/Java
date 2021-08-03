7c85df51ca37d6be78a73569b2b84740abff85f8
Mayank Saxena <saxenamayank0303@gmail.com>
2021-08-03 19:52:32 +0530

    Fixed MethodChaining.java

--- MethodChaining.java
+++ MethodChaining.java
@@ -1,6 +1,13 @@
 public class MethodChaining {
     public static void main(String[] args) {
         //Multiple methods in one line is called method chaining
-        //Refer after this keyword
+        String a = "MayaNk";
+        a = a.toLowerCase();
+        a = a.concat(" is a good boy");
+        System.out.println(a);
+        String b = "HarSh";
+        //We can call toLowerCase Method and concat Method in same line.
+        b = b.toLowerCase().concat(" is also a good boy"); //Both method are called in single line. It reduces the length of code.
+        System.out.println(b);
     }
 }
